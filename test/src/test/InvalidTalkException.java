a38971f56d91afc6c236275da4f4039eaa34debe
Parshya2212 <58530348+Parshya2212@users.noreply.github.com>
2019-12-05 01:35:15 +0530

    Add files via upload

---
+++ test/src/test/InvalidTalkException.java
@@ -0,0 +1,9 @@
+package test;
+
+public class InvalidTalkException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+    public InvalidTalkException(String msg) {
+        super(msg);
+    }
+}
